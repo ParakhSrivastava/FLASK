194d27402ecb000c474ad39213ef2342ea4fd9d5
Parakh Srivastava <srivastavaparakh2017@gmail.com>
2020-01-06 14:36:44 +0530

    Add files via upload

---
+++ 05_Templates/01_Basic-Template.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+# 'templates' should be the folder name
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # Connecting to a template (html file)
+    return render_template('00-Basic-Template.html')
+
+if __name__ == '__main__':
+    app.run()
