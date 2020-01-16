c94f61b3aab2ca1be5047425a22757744b1f06b9
Parakh Srivastava <srivastavaparakh2017@gmail.com>
2020-01-16 18:43:21 +0530

    Add files via upload

---
+++ 08_User-Authentication/03_Using-Flask-OAuth/app.py
@@ -0,0 +1,49 @@
+import os
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = '1'
+os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = '1'
+
+from flask import Flask, url_for, render_template
+from flask_dance.contrib.google import make_google_blueprint, google
+
+app = Flask(__name__)
+
+
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+
+blueprint = make_google_blueprint(
+    client_id="######.apps.googleusercontent.com",
+    client_secret="####",
+    # reprompt_consent=True,
+    offline=True,
+    scope=["profile", "email"]
+)
+app.register_blueprint(blueprint, url_prefix="/login")
+
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+@app.route('/welcome')
+def welcome():
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    email=resp.json()["email"]
+
+    return render_template("welcome.html",email=email)
+
+@app.route("/login/google")
+def login():
+    if not google.authorized:
+        return render_template(url_for("google.login"))
+
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    email=resp.json()["email"]
+
+    return render_template("welcome.html",email=email)
+
+
+if __name__ == "__main__":
+    app.run()
